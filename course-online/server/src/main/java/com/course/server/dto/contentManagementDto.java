4a951d1223d67317a3429aa4936f23d27a43679d
zjw <jingwang.zhang@supwisdom.com>
2023-05-05 16:10:07 +0800

    First Commit

---
+++ course-online/server/src/main/java/com/course/server/dto/contentManagementDto.java
@@ -0,0 +1,175 @@
+package com.course.server.dto;
+
+import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class contentManagementDto {
+
+    /**
+     * 主键
+     */
+    private String id;
+
+    /**
+     * 标题
+     */
+    private String title;
+
+    /**
+     * 内容
+     */
+    private String content;
+
+    /**
+     * 附件id集合
+     */
+    private String ids;
+
+    /**
+     * 一级栏目
+     */
+    private String firstColumn;
+
+    /**
+     * 二级栏目
+     */
+    private String secondColumn;
+
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date updateTime;
+
+    /**
+     * 创建人
+     */
+    private String createBy;
+
+    /**
+     * 更新人
+     */
+    private String updateBy;
+
+    /**
+     * 是否已删除,0未删除，1已删除
+     */
+    private Integer isDel;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getIds() {
+        return ids;
+    }
+
+    public void setIds(String ids) {
+        this.ids = ids;
+    }
+
+    public String getFirstColumn() {
+        return firstColumn;
+    }
+
+    public void setFirstColumn(String firstColumn) {
+        this.firstColumn = firstColumn;
+    }
+
+    public String getSecondColumn() {
+        return secondColumn;
+    }
+
+    public void setSecondColumn(String secondColumn) {
+        this.secondColumn = secondColumn;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public Integer getIsDel() {
+        return isDel;
+    }
+
+    public void setIsDel(Integer isDel) {
+        this.isDel = isDel;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", title=").append(title);
+        sb.append(", content=").append(content);
+        sb.append(", ids=").append(ids);
+        sb.append(", firstColumn=").append(firstColumn);
+        sb.append(", secondColumn=").append(secondColumn);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", updateTime=").append(updateTime);
+        sb.append(", createBy=").append(createBy);
+        sb.append(", updateBy=").append(updateBy);
+        sb.append(", isDel=").append(isDel);
+        sb.append("]");
+        return sb.toString();
+    }
+
+}
